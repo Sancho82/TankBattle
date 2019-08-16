c7902c451bc03ad7342ffc4cf7395f32b6783ccb
szani <gabor.szaniszlo82@gmail.com>
2019-08-16 20:47:10 +0200

    GameColors class finished

--- src/colors/GameColors.java
+++ src/colors/GameColors.java
@@ -1,5 +1,95 @@
 package colors;
 
+import java.awt.*;
+
 public class GameColors {
 
+    private Color alarm = new Color(210, 50, 50);
+    private Color bleed = new Color(250, 60, 30);
+    private Color roast = new Color(220, 90, 30);
+    private Color rare = new Color(220, 135, 30);
+    private Color gold = new Color(220, 180, 30);
+    private Color solar = new Color(240, 240, 30);
+    private Color trepp = new Color(150, 220, 30);
+    private Color grass = new Color(90, 240, 70);
+    private Color life = new Color(30, 220, 50);
+    private Color drape = new Color(250, 240, 130);
+    private Color peach = new Color(240, 160, 130);
+    private Color dream = new Color(250, 150, 150);
+    private Color fog = new Color(120, 170, 170);
+    private Color ocean = new Color(80, 80, 240);
+
+    // private Color usMilitary = new Color(75, 83, 32);
+    private Color usMilitary = new Color(154, 130, 108);
+    private Color germanMilitary = new Color(77, 93, 83);
+    private Color sovietMilitary = new Color(210, 50, 50);
+
+    public Color getUsMilitary() {
+        return usMilitary;
+    }
+
+    public Color getGermanMilitary() {
+        return germanMilitary;
+    }
+
+    public Color getSovietMilitary() {
+        return sovietMilitary;
+    }
+
+
+    public Color getAlarm() {
+        return  alarm;
+    }
+
+    public Color getBleed() {
+        return  bleed;
+    }
+
+    public Color getRoast() {
+        return  roast;
+    }
+
+    public Color getGold() {
+        return  gold;
+    }
+
+    public Color getSolar() {
+        return  solar;
+    }
+
+    public Color getTrepp() {
+        return  trepp;
+    }
+
+    public Color getGrass() {
+        return  grass;
+    }
+
+    public Color getLife() {
+        return  life;
+    }
+
+    public Color getDrape() {
+        return  drape;
+    }
+
+    public Color getRare() {
+        return  rare;
+    }
+
+    public Color getPeach() {
+        return  peach;
+    }
+
+    public Color getDream() {
+        return  dream;
+    }
+
+    public Color getFog() {
+        return  fog;
+    }
+
+    public Color getOcean() {
+        return  ocean;
+    }
 }
