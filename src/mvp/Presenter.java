af20b26928d420d2b6ba415931b51056bed06582
szani <gabor.szaniszlo82@gmail.com>
2019-08-16 21:10:31 +0200

    worked on Presenter Class

--- src/mvp/Presenter.java
+++ src/mvp/Presenter.java
@@ -2,6 +2,13 @@ package mvp;
 
 public class Presenter implements Contract.PresenterIF {
 
+    private View view;
+    private Logic logic;
+    
+    public Presenter(View view) {
+        this.view = view;
+    }
+
     @Override
     public void startGame(String message, int tanksPerArmy) {
 
