fc28e6f8032d466f1db161600fa9c90aa1d7902f
szani <gabor.szaniszlo82@gmail.com>
2019-08-16 21:19:39 +0200

    Presenter class finished

--- src/mvp/Presenter.java
+++ src/mvp/Presenter.java
@@ -1,9 +1,13 @@
 package mvp;
 
+import accessories.Tasker;
+
 public class Presenter implements Contract.PresenterIF {
 
     private View view;
     private Logic logic;
+
+    private Tasker tasker;
 
     public Presenter(View view) {
         this.view = view;
@@ -12,6 +16,7 @@ public class Presenter implements Contract.PresenterIF {
     public void startGame(String message, int tanksPerArmy) {
 
         logic = new Logic(message, tanksPerArmy);
+        tasker = new Tasker(view, logic);
 
         view.setMapScreen();
         logic.getLoader().load(logic.getTankMatrix(), logic.getTankList(), logic.getCreator());
@@ -20,12 +25,12 @@ public class Presenter implements Contract.PresenterIF {
         tankMovementController();
     }
 
-    @Override
     public void tankMovementController() {
-
+        tasker.getTimer().scheduleAtFixedRate(tasker, tasker.getDate(), 1000);
     }
 
     public void finishGame() {
+        tasker.getTimer().cancel();
         view.setEndScreen(logic.checkWinner());
     }
 }
